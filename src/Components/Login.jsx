17f7fd3dddf8c8525fe2f15fa03dc564aa347cd9
Klynic <155239907+Klynic@users.noreply.github.com>
2024-07-02 08:06:43 +0100

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -16,7 +16,7 @@ const Login = ({connectWallet, account}) => {
             <div className='flex lg:px-[30rem] w-full '>
 
                 <div className='lg:w-full text-center justify-center h-full pt-[10rem] px-4 '>
-                    <h2 className='lg:text-5xl text-4xl font-semibold leading-tight'>Electoral commission of <br /> Namibia(ECN)</h2>
+                    <h2 className='lg:text-5xl text-4xl font-semibold leading-tight'>Electoral Commission of <br /> Namibia(ECN)</h2>
                     <p className='py-5 px-4'>Vote securely on the blockchain and protect your vote</p>
                     
                     <button onClick = {connectWallet} class="btn btn-active lg:w-full w-1/2 text-white lg:text-sm  btn-primary bg-gradient-to-r
